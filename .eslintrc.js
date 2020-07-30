0facf5ba34a1969fe5b4300fbacd4d19f7f9035b
Brett Bloxom <brettbloxom@gmail.com>
2020-07-30 12:58:53 -0700

    ignore rest siblings

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,7 +36,7 @@ module.exports = {
       "no-unused-vars": [
         1,
         {
-          "ignoreSiblings": true
+          "ignoreRestSiblings": true
         }
       ],
       "prefer-const": [
